38ce0fe0f2fd35d2d3abfe4da15aca7951d8bbb4
Marcelo Lessa <mlessaribeiro@gmail.com>
2020-07-03 15:22:26 -0300

    Maven example

--- vars/pipelineMaven.groovy
+++ vars/pipelineMaven.groovy
@@ -6,7 +6,10 @@ def call(String repository) {
             sh "git clone " + repository
         }
         stage('Compile') {
-            sh "cd calc | mvn -Dmaven.test.skip=true clean install "
+            sh """
+                cd calc
+                mvn -Dmaven.test.skip=true clean install
+            """            
         }
         stage('Test') {
             sh "mvn -Dmaven.test.skip=true clean install "
