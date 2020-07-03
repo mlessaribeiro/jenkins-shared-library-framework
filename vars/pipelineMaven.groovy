855b0745a61af8dc5e12a2c2504e7961328003df
Marcelo Lessa <mlessaribeiro@gmail.com>
2020-07-03 15:12:09 -0300

    Maven example

--- vars/pipelineMaven.groovy
+++ vars/pipelineMaven.groovy
@@ -2,23 +2,21 @@
 def call(String repository) {
     node {
         stage('Scm') {
-            sh "rm -r job"
-            sh "mkdir job"
-            sh "cd job"
+            sh "rm -r calc"
             sh "git clone " + repository
         }
         stage('Compile') {
             sh "mvn -Dmaven.test.skip=true clean install "
         }
-        // stage('Test') {
-        //     sh "mvn -Dmaven.test.skip=true clean install "
-        // }
-        // stage('Build') {
-        //     sh "mvn -Dmaven.test.skip=true package "
-        // }        
-        // stage('Deploy') {
-        //     echo 'Fazendo deploy na AWS'
-        // }
+        stage('Test') {
+            sh "mvn -Dmaven.test.skip=true clean install "
+        }
+        stage('Build') {
+            sh "mvn -Dmaven.test.skip=true package "
+        }        
+        stage('Deploy') {
+            echo 'Fazendo deploy na AWS'
+        }
         // body()
     }    
 }
